e45abd00c873db8c9887ae6185d80bf86e7eda26
flaesteiro <esteiroflavia@gmail.com>
2023-06-16 16:36:07 -0300

    primero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  let[count,setCount] = useState(0)
+  useEffect(()=>{
+    console.log("montado de componente")
+    return(console.log("desmontado de componente"))
+  },[])
+  useEffect(()=>{
+    console.log("actualizacion de componente")
+  },[count])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
+      <span>{count}</span>
+      <button onClick={()=>setCount(count++)}>Click</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
